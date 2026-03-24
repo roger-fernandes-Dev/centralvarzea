647abdc9858424287b6395d7a5813efab8949ff1
roger-fernandes-Dev <rogerfernandes.developer@gmail.com>
2026-03-24 18:57:51 -0300

    alteracao da pagina do time aguias de deus

--- app/clubs/aguias-de-deus/page.tsx
+++ app/clubs/aguias-de-deus/page.tsx
@@ -1,8 +1,6 @@
+import BannerGeralTime from "@/components/home/BannerGeralTime"
 export default function AguiasDeDeus(){
     return(
-        <main>
-            <h2>Aguardando o contato</h2>
-            <h4>Contate o adm</h4>
-        </main>
+        <BannerGeralTime />
     )
 }
