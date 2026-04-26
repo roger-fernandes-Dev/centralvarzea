faba0f205a6c0670e029a31ade9cb1fed70ad0f1
roger-fernandes-Dev <rogerfernandes.developer@gmail.com>
2026-04-26 15:32:17 -0300

    resultados copa adc

--- app/lib/championship.ts
+++ app/lib/championship.ts
@@ -18,7 +18,7 @@ export const championship = [
       { home: "ADS", away: "MXE", date: "12/04", time: "08:00", homeScore: 4, awayScore: 3 },
       { home: "MEC", away: "AFA", date: "12/04", time: "08:00", homeScore: 4, awayScore: 2 },
       { home: "MEC", away: "MXE", date: "26/04", time: "08:00", homeScore: 2, awayScore: 0 },
-      { home: "AFA", away: "ADS", date: "26/04", time: "09:10", homeScore: 1, awayScore: 3 },
+      { home: "AFA", away: "ADS", date: "26/04", time: "09:10", homeScore: 3, awayScore: 1 },
     ],
   },
   {
