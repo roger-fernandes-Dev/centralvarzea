a0386694b77ec299482ccbb07afb4f99ba21716a
roger-fernandes-Dev <rogerfernandes.developer@gmail.com>
2026-05-17 21:20:07 -0300

    ajustes login

--- app/api/admin/news/route.ts
+++ app/api/admin/news/route.ts
@@ -32,17 +32,19 @@ export async function POST(req: Request) {
     // =========================
     const cookieStore = await cookies()
 
-const supabaseAuth = createServerClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    cookies: {
-      getAll() {
-        return cookieStore.getAll()
-      },
-      setAll(cookiesToSet) {
-        cookiesToSet.forEach(({ name, value, options }) => {
-          cookieStore.set(name, value, options)
+    console.log("COOKIES:", cookieStore.getAll())
+
+    const supabaseAuth = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return cookieStore.getAll()
+          },
+          setAll(cookiesToSet) {
+            cookiesToSet.forEach(({ name, value, options }) => {
+              cookieStore.set(name, value, options)
             })
           },
         },
@@ -51,7 +53,11 @@ const supabaseAuth = createServerClient(
 
     const {
       data: { user },
+      error: userError,
     } = await supabaseAuth.auth.getUser()
+
+    console.log("USER:", user)
+    console.log("USER ERROR:", userError)
 
     if (!user) {
       return NextResponse.json(
