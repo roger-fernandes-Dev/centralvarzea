a22bf8865553d57f58e1bdce79a30b01d67db635
roger-fernandes-Dev <rogerfernandes.developer@gmail.com>
2026-06-21 10:18:56 -0300

    alteração  middleware

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,15 @@ import { NextResponse, type NextRequest } from "next/server"
 export async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname
 
-  return NextResponse.next()
-}
-// bloqueia login jogador
+  // bloqueia login jogador
   if (pathname === "/login") {
     return NextResponse.redirect(new URL("/", req.url))
   }
+
+  return NextResponse.next()
+  
+}
+
 export const config = {
   matcher: ["/login"],
 }
